f1e584b1534fa982c6cced64cf175d210669a1d2
Smirnova <42230707+MurkyMeow@users.noreply.github.com>
2025-01-05 23:50:46 +0300

    Update shard_overlay.py

--- pytoniq/adnl/overlay/shard_overlay.py
+++ pytoniq/adnl/overlay/shard_overlay.py
@@ -29,7 +29,7 @@ class ShardOverlay:
         if self.external_messages_handler is not None:
             self._overlay.set_broadcast_handler('tonNode.externalMessageBroadcast', self.external_messages_handler)
         if self.shard_blocks_handler is not None:
-            self._overlay.set_broadcast_handler('tonNode.newShardBlockBroadcast', self.shard_blocks_handler)
+            self._overlay.set_broadcast_handler(None, self.shard_blocks_handler)
 
     async def send_external_message(self, message: bytes):
         data = {'@type': 'tonNode.externalMessageBroadcast', 'message': {
